9ecf869fd69ad06ca55fa1209bfd9c6b5f16f861
Miqueas Ledesma <Mickeas98@gmail.com>
2022-09-26 18:43:31 -0300

    fix-newfolder-database

--- server/models/Brand.js
+++ server/models/Brand.js
@@ -1,10 +1,12 @@
-const {DataTypes} = require('sequelize');
+const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize)=>{
     sequelize.define(
         'brand',
         {
-
+        name:{
+            type: DataTypes.STRING,
+            allowNull: false
         }
         },
     { timestamps: false }
