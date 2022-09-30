079a5c200308029f1d702c7ab1fcdfbb844377c0
Miqueas Ledesma <Mickeas98@gmail.com>
2022-09-30 18:17:31 -0300

    refact-filter/querys

--- controllers/filterController.js
+++ controllers/filterController.js
@@ -1,218 +1,44 @@
+const { Op } = require('sequelize');
 const { Product, Category, Brand, Image } = require('../server/database/db');
 
-// Es altamente posible que tenga que agregar otra funcion mas para hacer el paginado asique no es version final
-
 const sortAndFilter = async (req, res) => {
-    const { categoryId, brandId, sortBy } = req.body;
+    const { categoryId, brandId } = req.query;
     const pageAsNumber = Number.parseInt(req.query.page);
     const sizeAsNumber = Number.parseInt(req.query.size);
+    let type = req.query.type || 'id';
+    let sort = req.query.sort || 'DESC';
+    let category = categoryId || {[Op.between]: [1, 20]};
+    let brand = brandId || {[Op.between]: [1, 20]};
     let page = 0;
     let size = 10;
+    console.log(type, sort, category, brand);
     if (!Number.isNaN(pageAsNumber) && pageAsNumber >= 0) page = pageAsNumber;
     if (!Number.isNaN(sizeAsNumber) && sizeAsNumber >= 1) size = sizeAsNumber;
-    if (!sortBy) {
-        if (!categoryId && !brandId) {
-            try {
-                await Product.findAndCountAll({
-                    limit: size,
-                    offset: page * size,
-                    include: [
-                        Category,
-                        Brand,
-                        Image
-                    ],
-                })
-                    .then(r => res.send({
-                        content: r.rows,
-                        totalPage: Math.ceil(r.count / size)
-                    }));
-            } catch (error) {
-                console.log(error)
-                return res.status(400).send('failed!')
-            }
-        };
-
-        if (categoryId && !brandId) {
-            try {
-                await Product.findAndCountAll({
-                    where: {
-                        categoryId,
-                    },
-                    limit: size,
-                    offset: page * size,
-                    include: [
-                        Category,
-                        Brand,
-                        Image
-                    ]
-                })
-                    .then(r => res.send({
-                        content: r.rows,
-                        totalPage: Math.ceil((r.count / size))
-                    }))
-            } catch (error) {
-                console.log(error)
-                return res.status(400).send('failed!')
-            };
-        };
-
-        if (brandId && !categoryId) {
-            try {
-                await Product.findAndCountAll({
-                    where: {
-                        brandId,
-                    },
-                    limit: size,
-                    offset: page * size,
-                    include: [
-                        Category,
-                        Brand,
-                        Image
-                    ]
-                })
-                    .then(r => res.send({
-                        content: r.rows,
-                        totalPage: Math.ceil((r.count / size))
-                    }))
-            } catch (error) {
-                console.log(error)
-                return res.status(400).send('failed!')
-            };
-        };
-
-        if (brandId && categoryId) {
-            try {
-                await Product.findAndCountAll({
-                    where: {
-                        brandId,
-                        categoryId
-                    },
-                    limit: size,
-                    offset: page * size,
-                    include: [
-                        Category,
-                        Brand,
-                        Image
-                    ]
-                })
-                    .then(r => res.send({
-                        content: r.rows,
-                        totalPage: Math.ceil((r.count / size))
-                    }))
-            } catch (error) {
-                console.log(error)
-                return res.status(400).send('failed!')
-            }
-        }
-    } else {
-        let { type, sort } = sortBy;
-        if (!type) type = 'salePrice';
-        if(!sort) sort = 'DESC';
-        if (!categoryId && !brandId) {
-            try {
-                await Product.findAndCountAll({
-                    limit: size,
-                    offset: page * size,
-                    include: [
-                        Category,
-                        Brand,
-                        Image
-                    ],
-                    order: [
-                        [type, sort]
-                    ]
-                })
-                    .then(r => res.send({
-                        content: r.rows,
-                        totalPage: Math.ceil(r.count / size)
-                    }));
-            } catch (error) {
-                console.log(error)
-                return res.status(400).send('failed!')
-            }
-        };
-
-        if (categoryId && !brandId) {
-            try {
-                await Product.findAndCountAll({
-                    where: {
-                        categoryId,
-                    },
-                    limit: size,
-                    offset: page * size,
-                    include: [
-                        Category,
-                        Brand,
-                        Image
-                    ],
-                    order: [
-                        [type, sort]
-                    ]
-                })
-                    .then(r => res.send({
-                        content: r.rows,
-                        totalPage: Math.ceil((r.count / size))
-                    }))
-            } catch (error) {
-                console.log(error)
-                return res.status(400).send('failed!')
-            };
-        };
-
-        if (brandId && !categoryId) {
-            try {
-                await Product.findAndCountAll({
-                    where: {
-                        brandId,
-                    },
-                    limit: size,
-                    offset: page * size,
-                    include: [
-                        Category,
-                        Brand,
-                        Image
-                    ],
-                    order: [
-                        [type, sort]
-                    ]
-                })
-                    .then(r => res.send({
-                        content: r.rows,
-                        totalPage: Math.ceil((r.count / size))
-                    }))
-            } catch (error) {
-                console.log(error)
-                return res.status(400).send('failed!')
-            };
-        };
-
-        if (brandId && categoryId) {
-            try {
-                await Product.findAndCountAll({
-                    where: {
-                        brandId,
-                        categoryId
-                    },
-                    limit: size,
-                    offset: page * size,
-                    include: [
-                        Category,
-                        Brand,
-                        Image
-                    ],
-                    order: [
-                        [type, sort]
-                    ]
-                })
-                    .then(r => res.send({
-                        content: r.rows,
-                        totalPage: Math.ceil((r.count / size))
-                    }))
-            } catch (error) {
-                console.log(error)
-                return res.status(400).send('failed!')
-            }
-        }
+    try {
+        await Product.findAndCountAll({
+            where:{
+                categoryId: category,
+                brandId: brand 
+            },
+            limit: size,
+            offset: page * size,
+            include:[
+                Category,
+                Brand,
+                Image
+            ],
+            order:[
+                [type, sort]
+            ]
+        }).then(r => {
+            return res.send({
+                content:r.rows,
+                totalPage: Math.ceil((r.count / size))
+            })
+        })
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).send(error.message);
     }
 }
 
