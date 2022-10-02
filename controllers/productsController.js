dfddbc793119f6acdc9c01a873475014aed765a5
Billy Mendieta <oscarald.mc@gmail.com>
2022-10-02 17:03:07 -0400

    fix/post-products

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -83,14 +83,14 @@ const getProducts = async (req, res) => {
 
 const postProduct = async (req, res) => {
 
-    const { name, description, purchasePrice, salePrice, stock, status, brand, category, rating } = req.body;
+    const { name, description, purchasePrice, salePrice, stock, brand, category} = req.body;
     try {
-        if (!name || !description || !purchasePrice || !salePrice || !stock || !status || !rating) {
+        if (!name || !description || !purchasePrice || !salePrice || !stock ) {
             return res.send('information is missing!') // Cambie el mensaje, significa 'falta informacion!'
         }
 
         let allProduct = await Product.findAll()
-        let aProduct = allProduct.find(e => e.name.toLowerCase() === name.toLowerCase() && e.status === status)
+        let aProduct = allProduct.find(e => e.name.toLowerCase() === name.toLowerCase())
 
         if (aProduct) {
             return res.send('the product exist!')
@@ -108,10 +108,8 @@ const postProduct = async (req, res) => {
             purchasePrice,
             salePrice,
             stock,
-            status,
             brand,
             category,
-            rating,
             brandId: brandDb.id,
             categoryId: categoryDb.id
         })
