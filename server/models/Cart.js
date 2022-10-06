f6a123a3fc984014830fccda64a3d3bceea48bc2
Jander Gomez <jandergb.30@gmail.com>
2022-10-06 00:03:55 -0500

    feat: add cartShopping

---
+++ server/models/Cart.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize");
+module.exports = function(sequelize) {
+    sequelize.define (
+        'cart',
+        {
+            email: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            salePrice: {
+                type: DataTypes.FLOAT,
+                allowNull: true,
+            },
+            image: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            quantity: {
+                type: DataTypes.INTEGER,
+                allowNull: true,
+            },
+            discount: {
+                type: DataTypes.FLOAT,
+                allowNull: true,
+            },
+            subTotal: {
+                type: DataTypes.FLOAT,
+                allowNull: true,
+            },
+        }
+    )
+}
