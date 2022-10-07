0e8556f9d97b490a1b4264e49f3e30bda189e744
Miqueas Ledesma <104766422+MiqueasLedesma@users.noreply.github.com>
2022-10-07 19:47:32 -0300

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -14,29 +14,29 @@ const sequelize = new Sequelize(
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, NODE_ENV } = process.env;
 
 const sequelize =
-    // NODE_ENV === "develop"
-    //     ? new Sequelize({
-    //         database: DB_NAME,
-    //         dialect: "postgres",
-    //         host: DB_HOST,
-    //         port: 5432,
-    //         username: DB_USER,
-    //         password: DB_PASSWORD,
-    //         pool: {
-    //             max: 3,
-    //             min: 1,
-    //             idle: 10000
-    //         },
-    //         dialectOptions: {
-    //             ssl: {
-    //                 require: true,
-    //                 rejectUnauthorized: false
-    //             },
-    //             keepAlive:true,
-    //         },
-    //         ssl: true,
-    //     })
-    //     : 
+    NODE_ENV === "develop"
+        ? new Sequelize({
+            database: DB_NAME,
+            dialect: "postgres",
+            host: DB_HOST,
+            port: 5432,
+            username: DB_USER,
+            password: DB_PASSWORD,
+            pool: {
+                max: 3,
+                min: 1,
+                idle: 10000
+            },
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                },
+                keepAlive:true,
+            },
+            ssl: true,
+        })
+        : 
     new Sequelize(
             `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
             {
