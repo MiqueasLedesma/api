a6b71d1828a88b156594ce0caa0c1bb7e3acf36b
Miqueas Ledesma <Mickeas98@gmail.com>
2022-10-06 21:20:56 -0300

    feat-endpoints/reviews

---
+++ controllers/reviewController.js
@@ -0,0 +1,141 @@
+const { Review } = require('../server/database/db');
+
+const postReview = async (req, res) => {
+    const { productId, stars, detail, userId } = req.body;
+    if (!productId || !stars || !detail || !userId) return res.status(400).send('All camps are obligatories!');
+    const repetidos = await Review.findAll({
+        where: {
+            userId,
+            productId
+        }
+    })
+    if (!repetidos[0]) {
+        try {
+            await Review.create({
+                productId,
+                userId,
+                detail,
+                stars
+            })
+                .then(r => res.send('Review has been created!'))
+        } catch (error) {
+            console.log(error.message);
+            return res.status(400).send(error.message);
+        };
+    } else {
+        return res.status(400).send('This user already has a review for this product!')
+    };
+};
+
+const getReviews = async (req, res) => {
+    const { productId } = req.query;
+    const isANumber = /^([0-9])*$/;
+    if (!isANumber.test(productId)) return res.status(400).send('Id must be a number!');
+    let page = !isANumber.test(req.query.page) ? 0 : req.query.page;
+    try {
+        await Review.findAndCountAll({
+            where: {
+                status: true
+            },
+            limit: 2,
+            offset: page * 2
+        })
+            .then(r => res.send({
+                content: r.rows,
+                totalPage: Math.ceil(r.count / (2 * 2))
+            }));
+    } catch (error) {
+        console.log(error.message);
+        return res.status(400).send(error.message);
+    };
+};
+
+const changeReview = async (req, res) => {
+    const { id, detail, stars } = req.body;
+    const isANumber = /^([0-9])*$/;
+    if (!id || !detail || !stars) return res.status(400).send('All camps are obligatories!');
+    if (id && !isANumber.test(id)) return res.status(400).send('Id must be a number!');
+    try {
+        await Review.update({
+            detail,
+            stars
+        }, {
+            where: {
+                id
+            }
+        })
+            .then(r => res.send('Review has been updated!'));
+    } catch (error) {
+        console.log(error.message);
+        return res.status(400).send(error.message);
+    };
+};
+
+const deleteReview = async (req, res) => {
+    const { id } = req.query;
+    const isANumber = /^([0-9])*$/;
+    if (id && !isANumber.test(id)) return res.status(400).send('Id must be a number!');
+    try {
+        await Review.update({
+            status: false
+        }, {
+            where: {
+                id
+            }
+        })
+            .then(r => res.send('Review has been deleted!'));
+    } catch (error) {
+        console.log(error.message);
+        return res.status(400).send(error.message);
+    };
+};
+
+const revertDeleteReview = async (req, res) => {
+    const { id } = req.query;
+    const isANumber = /^([0-9])*$/;
+    if (id && !isANumber.test(id)) return res.status(400).send('Id must be a number!');
+    try {
+        await Review.update({
+            status: true
+        }, {
+            where: {
+                id
+            }
+        })
+            .then(r => res.send('Review has been updated!'))
+    } catch (error) {
+        console.log(error.message);
+        return res.status(400).send(error.message);
+    };
+};
+
+const getAllDeletedReviews = async (req, res) => {
+    const isANumber = /^([0-9])*$/;
+    let page = !isANumber.test(req.query.page) ? 0 : req.query.page;
+    try {
+        await Review.findAndCountAll({
+            limit: 2,
+            offset: page * 2,
+            where: {
+                status: false
+            }
+        })
+            .then(r => res.send({
+                content: r.rows,
+                totalPage: Math.ceil(r.count / (2 * 2))
+            }))
+    } catch (error) {
+        console.log(error.message);
+        return res.status(400).send(error.message);
+    };
+};
+
+
+module.exports = {
+    postReview,
+    getReviews,
+    changeReview,
+    deleteReview,
+    revertDeleteReview,
+    getAllDeletedReviews
+};
