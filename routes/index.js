9830568631e437077d69ed00c91ea0e76fcd60ea
Miqueas Ledesma <104766422+MiqueasLedesma@users.noreply.github.com>
2022-10-14 01:38:08 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,7 @@ const routeSucursal = require("./routeSucursals")
 const routeEmails = require("./routeEmails");
 const routeGeolocation = require("./routeGeolocation");
 
+
 router.use('/products', routeProducts);
 router.use('/filter', routeFilter);
 router.use('/categorys', routeCategorys);
@@ -28,6 +29,8 @@ router.use('/payments', routePayments)
 router.use('/email',routeEmails)
 router.use('/orders', routeOrders);
 router.use('/sucursal', routeSucursal);
-router.use('/geo', routeGeolocation)
+router.use('/geo', routeGeolocation);
+router.use('/users', routeUsers);
+
 
 module.exports = router;
